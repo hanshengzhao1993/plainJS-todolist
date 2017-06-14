fa48d1c570fa10c6f6e07ccb1d4fd316337edc2a
Jason Chambers <jas.o.chambers@gmail.com>
2017-06-14 15:42:41 -0700

    (feat) append todo to list on button click

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
-(function(something) {
-  console.log(something, 'this is the something <==');
-})(this);
+
+function addTodo() {
+  var inputText = document.getElementById('input-box').value;
+  var ul = document.getElementById('todo-list');
+  if (inputText !== '') {
+    var li = document.createElement('li');
+    var textNode = document.createTextNode(inputText);
+    li.appendChild(textNode);  
+    ul.appendChild(li);
+    document.getElementById('input-box').value = '';
+  } else {
+    alert('Input text into field');
+  }
+
+}
