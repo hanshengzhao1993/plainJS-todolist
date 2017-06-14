06a0f6bfdd3e3e911651c241cbee2b9dedb13a3c
Jason Chambers <jas.o.chambers@gmail.com>
2017-06-14 16:30:35 -0700

    (feat) added basic styling for list

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('add-btn').addEventListener('click', addTodo, false);
+  document.getElementById('todo-list').addEventListener('click', toggleComplete, false);
+});
 
 function addTodo() {
   var inputText = document.getElementById('input-box').value;
@@ -11,5 +15,19 @@ function addTodo() {
   } else {
     alert('Input text into field');
   }
-
 }
+
+function removeNode(e) {
+  var parent = e.target.parentNode;
+  parent.removeChild(e.target);
+}
+
+function toggleComplete(e) {
+  var node = e.target;
+  node.classList.toggle('complete');
+}
+
+function addCheckElement() {
+  
+}
+
