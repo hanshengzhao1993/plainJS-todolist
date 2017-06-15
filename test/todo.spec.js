74b43ad29699a317aa23fe15cf12afc74ba28511
Jason Chambers <jas.o.chambers@gmail.com>
2017-06-14 17:53:46 -0700

    (feat) test for list item node removal

--- test/todo.spec.js
+++ test/todo.spec.js
@@ -1,6 +1,7 @@
 var expect = chai.expect;
 
 var $ = selector => document.querySelector(selector);
+var $all = selector => document.querySelectorAll(selector);
 
 describe('Todo List', function() {
 
@@ -8,6 +9,13 @@ describe('Todo List', function() {
     $('#input-box').value = 'A todo';
     $('#add-btn').click();
   });
+
+  afterEach(function() {
+    var deleteItems = $all('.container ul > li > span');
+    deleteItems.forEach(function(item) {
+      item.click();
+    });
+  })
 
   it('should add a todo when input box is filled', function() {
     expect($('ul > li')).to.exist;
@@ -19,6 +27,13 @@ describe('Todo List', function() {
     expect(li.classList.contains('complete')).to.be.true;
   });
 
-  
+  it('should remove li element on close click', function() {
+    var listItems = $all('.container ul > li');
+    expect(listItems).to.have.length(1);
+    var checkMark = $('.container ul > li > span');
+    checkMark.click();
+    var updatedListItems = $all('.container ul > li');
+    expect(updatedListItems).to.have.length(0);
+  });
 
 });
