34c7279b826a8e1f9eee11d1d0ec6a7fdf83b213
Jason Chambers <jas.o.chambers@gmail.com>
2017-06-14 17:42:40 -0700

    (feat) add tests for detecting li element

--- test/todo.spec.js
+++ test/todo.spec.js
@@ -1,9 +1,24 @@
 var expect = chai.expect;
 
+var $ = selector => document.querySelector(selector);
+
 describe('Todo List', function() {
 
-  it('run test suite', function() {
-    expect('yes').to.equal('yes');
+  beforeEach(function(){
+    $('#input-box').value = 'A todo';
+    $('#add-btn').click();
   });
+
+  it('should add a todo when input box is filled', function() {
+    expect($('ul > li')).to.exist;
+  });
+
+  it('should add complete className when li element is clicked', function() {
+    var li = $('.container ul > li');
+    li.click();
+    expect(li.classList.contains('complete')).to.be.true;
+  });
+
+  
 
 });
